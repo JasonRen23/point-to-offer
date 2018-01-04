43f5b370a80f54e9dc90cc38fd0c5918894039e7
JasonRen23 <zhicheng_ren@163.com>
2018-01-04 21:24:54 +0800

    first commit

---
+++ src/chapter2/FindInPartiallySortedMatrix.java
@@ -0,0 +1,32 @@
+package chapter2;
+
+
+
+public class FindInPartiallySortedMatrix {
+    public static boolean findInPartiallySortedMatrix(int target, int [][] array){
+        int rows = array.length;
+        int cols = array[0].length;
+        if(array==null||array.length==0||(array.length==1&&array[0].length==0)) return false;
+        int row = 0;
+        int col = cols - 1;
+        while(row < rows && col >= 0){
+            if(array[row][col] == target){
+                return true;
+
+            }
+            if(array[row][col] > target){
+                -- col;
+            }else{
+                ++ row;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1,2,8,9},{4,7,10,13},{6,8,11,15}};
+        System.out.println("Result is " + findInPartiallySortedMatrix(7, array));
+
+    }
+}
