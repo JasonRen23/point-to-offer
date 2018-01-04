e0cbceddaa6bd159c11b882d366e18de10b576c8
JasonRen23 <zhicheng_ren@163.com>
2018-01-04 23:10:32 +0800

    add RepalaceSpaces

---
+++ src/chapter2/ReplaceSpaces.java
@@ -0,0 +1,53 @@
+package chapter2;
+
+public class ReplaceSpaces {
+    public static String replaceSpace(StringBuffer str){
+        int originLength = str.length();
+        int spaceCount = 0;
+        //if the str is not common, return
+        if(str == null || originLength <= 0){
+            return str.toString();
+        }
+        //compute the count of space
+        for(int i  = 0; i < originLength; ++i){
+            if(str.charAt(i) == ' '){
+                spaceCount += 1;
+            }
+        }
+        //add %20 length to originLength
+        int newLength = originLength + spaceCount*2;
+        // if there is no space, return
+        if (newLength == str.length() ){
+            return str.toString();
+        }
+        for(int i  = 0; i < spaceCount*2; ++i){
+               str.append('a');
+        }
+
+        //replace the space
+        int indexOfOriginal = originLength - 1;
+        int indexOfNew = newLength - 1;
+        while(indexOfOriginal >= 0 && indexOfNew > indexOfOriginal){
+            if(str.charAt(indexOfOriginal) == ' '){
+                str.setCharAt(indexOfNew--,'0');
+                str.setCharAt(indexOfNew--,'2');
+                str.setCharAt(indexOfNew--,'%');
+            }else{
+                str.setCharAt(indexOfNew--, str.charAt(indexOfOriginal));
+            }
+            --indexOfOriginal;
+        }
+
+
+
+
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("We are happy.");
+        System.out.println(replaceSpace(str));
+
+    }
+
+}
